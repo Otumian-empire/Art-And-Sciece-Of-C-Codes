ba5fd0ec340926440c9cd420693070aeccf49e32
Otumian <popecan1000@gmail.com>
2019-07-07 22:22:38 +0000

    added 3 files on using rand()

---
+++ chapter8/randnumgenbtnXY.c
@@ -0,0 +1,18 @@
+/*
+randnumgenbtnXY.c 
+created on Sunday, 2019, 07 July
+By Otumian
+*/
+
+/*
+implementing a general function that generates a random number between ranges
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    
+    
+    return 0;
+}
