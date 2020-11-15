c2072afe02e74df77bc605529019a46c132f2d2d
otumian-empire <popecan1000@gmail.com>
2020-11-15 17:34:02 +0000

    Pointers
    pointer expression
    pointer arithmetic
    address arithmetic

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 // int square(int x);
 // void say_hello();
 // int factorial(int i);
+void swap(int *a, int *b);
 
 int main(void)
 {
@@ -55,30 +56,112 @@ int main(void)
     // An array is a data structure that stores a collection of related values
     // that are all the same type.
 
-    char grades[5]; // create an array of 5 grades
-    float scores[5] = {23.6, 67.67, 89.01, 90.34, 56.89};
+    // char grades[5]; // create an array of 5 grades
+    // float scores[5] = {23.6, 67.67, 89.01, 90.34, 56.89};
 
-    printf("The first score is %.2f\n", scores[0]);
-    scores[0] = 32.76;
-    printf("After certain considerations, the first score becomes %.2f\n", scores[0]);
+    // printf("The first score is %.2f\n", scores[0]);
+    // scores[0] = 32.76;
+    // printf("After certain considerations, the first score becomes %.2f\n", scores[0]);
 
     // looping through the array
-    for (int index = 0; index < 5; index++)
-    {
-        printf("%.2f\n", scores[index]);
-    }
+    // for (int index = 0; index < 5; index++)
+    // {
+    //     printf("%.2f\n", scores[index]);
+    // }
 
-    puts("--------------------------------------\n");
+    // puts("--------------------------------------\n");
 
-    for (int index = 0; index < 5; index++)
-    {
-        scores[index] -= 34;
-    }
+    // for (int index = 0; index < 5; index++)
+    // {
+    //     scores[index] -= 34;
+    // }
 
-    for (int index = 0; index < 5; index++)
-    {
-        printf("%.2f\n", scores[index]);
-    }
+    // for (int index = 0; index < 5; index++)
+    // {
+    //     printf("%.2f\n", scores[index]);
+    // }
+
+    // pointers
+    // C is designed to be a low-level language that can easily access memory locations
+    //  and perform memory-related operations.
+    // For instance, the scanf() function places the value entered by the user at
+    // the location, or address, of the variable. This is accomplished by using
+    // the & symbol.
+
+    // int num;
+    // printf("Enter a number: ");
+
+    // scanf("%d", &num);
+    // printf("%d\n", num);
+
+    // &num is the address of the variable num
+    // A memory address is given as a hexadecimal number.
+
+    // printing the address of num (%x, %X, %p)
+    // printf("The address of %d is %p and %x\n", num , &num, &num);
+
+    // A pointer is a variable that contains the address of another variable.
+    // In other words, it "points" to the location assigned to a variable and
+    // can indirectly access the variable.
+    // Pointers are declared using the * symbol and take the form:
+    // pointer_type *identifier
+
+    // int num = 3;
+    // printf("value of num: %d\n\n", num);
+    // printf("address of num: %p\n\n", &num);
+    // printf("value at address of num: %d\n\n", *(&num));
+
+    // int *ptr = &num;
+    // printf("value of ptr: %p\n\n", ptr);
+    // printf("value at ptr: %d\n\n", *ptr);
+    // printf("address of ptr: %p\n\n", &ptr);
+
+    // Pointers in Expressions
+    // int res;
+
+    // res = (*ptr)++;
+    // printf("%d\n", res);
+
+    // res = 2 * ++(*ptr);
+    // printf("%d\n", res);
+
+    // printf("%d, %d\n", num, (*ptr));
+
+    // Pointers are especially useful with arrays.
+    // An array declaration reserves a block of contiguous
+    // memory addresses for its elements.
+
+    // int some_numbers[5] = {2, 3, 6, 7, 4};
+    // int *ptr = NULL;
+
+    // ptr = some_numbers;
+    // ptr = &some_numbers[0];
+    // this basically assigns the first element in the array to ptr
+    // printf("%d\n", *ptr);
+    // printf("%p\n", some_numbers); // an array is a pointer
+
+    // looping through the array - the ptr using address arithmetic
+    // The array is a contiguous memory so the first address + 1 will
+    // lead to the second.. current addr + 1 will lead to the next
+    // remember that we rather increment the address and not value
+    // for (int index = 0; index < 5; index++)
+    // {
+    //     printf("%p + %d = %d\n", ptr, index, *(ptr + index));
+    // }
+
+    // Pointers and Functions
+    // Pointers greatly expand the possibilities for functions.
+    // No longer are we limited to returning one value. With pointer
+    // parameters, your functions can alter actual data rather than a
+    // copy of data.
+
+    // To change the actual values of variables, the calling statement
+    // passes addresses to pointer parameters in a function.
+
+    int x = 5, y = 9;
+    printf("X = %d, Y = %d\n", x, y);
+    swap(&x, &y);
+    printf("X = %d, Y = %d\n", x, y);
 
     return 0;
 }
@@ -107,3 +190,12 @@ int main(void)
 // {
 //     return x * x;
 // }
+
+void swap(int *a, int *b)
+{
+    int c;
+
+    c = *a;
+    *a = *b;
+    *b = c;
+}
