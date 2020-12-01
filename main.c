4cdeece94d72bb4f85d40e4667c72e2b00af54a6
otumian-empire <popecan1000@gmail.com>
2020-12-01 13:18:37 +0000

    struct

--- main.c
+++ main.c
@@ -1,111 +1,85 @@
 #include <stdio.h>
 
+// typedef struct
+// {
+//     char *name;
+//     char gender;
+//     char sex;
+// } employee;
+
+// void print_employee(employee e)
+// {
+//     printf("Full Name: %s\nGender: %c\nSex: %c\n",
+//            e.name, e.gender, e.sex);
+// }
+
+typedef struct
+{
+    char *name;
+} profile;
+
+void print_prof_ptr(profile *e)
+{
+    printf("Full Name: %s\n", e->name);
+    // printf("Full Name: %s\n", (*e).name);
+}
+
+void print_prof(profile e)
+{
+    printf("Full Name: %s\n", e.name);
+}
+
 int main()
 {
-    // strings
+    // Structures
+    /* A structure is a user-defined data type that groups related 
+    variables of different data types.
+    A structure declaration includes the keyword struct, a structure
+     tag for referencing the structure, and curly braces { } with a 
+     list of variable declarations called members.  */
 
-    /* A string in C is an array of characters that ends with
-    a NULL character '\0'. */
-    // char username[11] = "johndoe123";
-    // printf("%s\n", username);
+    /* struct Human
+    {
+        char full_name[100];
+        int age;
+        float weight, height;
+    }; */
 
-    /*  this is a string literal
-     When you provide a string literal to initialize the string,
-      the compiler automatically adds a NULL character '\0' to the
-     char array. */
+    /* A structure is also called a composite or aggregate data type.
+    Some languages refer to structures as records */
 
-    // char username1[11] = {'j', 'o', 'h', 'n', 'd', 'o', 'e', '1', '2', '3', '\0'};
-    // printf("%s\n", username1);
+    /* declare an instance of Human, initializing with curly braces */
+    /* struct Human john = {"Michael Asante", 34, 23.4, 45.6}; */
 
-    /* With this approach, the NULL character must be added explicitly.
-    Note that the characters are enclosed in single quotation marks. */
+    /* using curly braces after declaration */
+    /* struct Human peter;
+    peter = (struct Human){"Peter Kwabena Asante", 45, 45.4, 120.6}; */
 
-    /* A string pointer declaration such as char *str = "stuff"; is
-    considered a constant and cannot be changed from its initial value. */
+    /* Using named paramters */
+    /* struct Human hannah =
+        {
+            .full_name = "Hannah Deloris Mintah",
+            .age = 45,
+            .weight = 67.67,
+            .height = 35.09}; */
 
-    // char *username2 = "johndoe123";
-    // printf("%s\n", username2);
+    /* Accessing the members of a struct with the dot operator */
+    /* printf("Full Name: %s\nAge: %d\nWeight: %.2f\nHeight: %.2f\n",
+           hannah.full_name, hannah.age, hannah.weight, hannah.height); */
 
-    /* Standard Library string functions in <string.h>
-    strlen() - get length of a string
-    strcat() - merge two strings
-    strcpy() - copy one string to another
-    strlwr() - convert string to lower case
-    strupr() - convert string to upper case
-    strrev() - reverse string
-    strcmp() - compare two strings */
+    //Typedef
+    /* The typedef keyword creates a type definition that simplifies code
+     and makes a program easier to read. it eliminates the need to use the
+      keyword struct when declaring variables.  */
 
-    // string input
-    /* To retrieve a line of text or other string from the user, C 
-    provides the scanf(), gets(), and fgets() functions. */
+    // employee mic = {"Daniel Somonaz", 'M', 'F'};
+    // print_employee(mic);
 
-    // char fullname[10];
-    // printf("Enter your fullname: ");
-    // scanf("%s", fullname);
+    // Pointers to Structures
+    profile p = {.name="Santiago Doe"};
 
-    // printf("FullName: %s\n", fullname);
-
-    /* When scanf() is used to read a string, there is no need for & to access
-     the variable address because an array name acts as a pointer. */
-
-    // char fullname1[10];
-    // printf("Enter your fullname: ");
-    /* gets(fullname1); */
-    /* A better version of gets is fgets */
-    /*     fgets(fullname1, 12, stdin); */
-
-    /* This prevents buffer overflow, which happens when the string array isn't 
-    big enough for the typed text. */
-
-    /* printf("FullName: %s\n", fullname1); */
-
-    /* char full_name[50];
-    printf("Enter your full name: ");
-    fgets(full_name, 50, stdin);
-    printf("%s\n", full_name); */
-
-    // string output
-
-    /* String output is handled with the fputs(), puts(), and printf() functions. */
-    /* We have seen several use of printf */
-
-    /*  char password[10];
-    char *text = "Enter pwd: ";
-    fputs(text, stdout);
-    fgets(password, 11, stdin); */
-    /* fputs(password, stdout); */
-    /* puts(password); */ /* Adds new line by default */
-
-    //String functions
-
-    /* The sprintf and sscanf Functions
-    sprintf is like a string builder - you take a couple of vars and create one 
-    sscanf is like a string splitter - you take a whole var and break it into a
-     couple of others  */
-
-    /* char sentence[100];
-    char firstname[10], lastname[10];
-    int age;
-    float weight;
-
-    printf("Enter fullname, age and weigh: ");
-    scanf("%s %s %d %f", firstname, lastname, &age, &weight);
-
-    sprintf(sentence, "My name is %s %s, I am %d years and I weigh, %.2f",
-            firstname, lastname, age, weight);
-
-    puts(sentence); */
-
-    /* Now we can break down a sentence into fragments with sscanf */
-
-    char *dist = "john doe 32 34.2";
-    char dfirstname[10], dlastname[10];
-    int dage;
-    float dweight;
-
-    sscanf(dist, "%s %s %d %f", dfirstname, dlastname, &dage, &dweight);
-    printf("Firstname: %s\nLastname: %s\nAge: %d\nWeight: %.2f\n",
-           dfirstname, dlastname, dage, dweight);
+    print_prof(p);
+    print_prof_ptr(&p);
 
     return 0;
 }
